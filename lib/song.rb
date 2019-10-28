79601af8a7131232bc7daa2a0f194de6959c00f6
Andrew Capp <acapp909@gmail.com>
2019-10-28 01:58:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,8 +35,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    create_by_name(name) if find_by_name(name)
-    binding.pry
+    if find_by_name(name)
+      find_by_name(name)
+    else
+      create_by_name(name)
+    #binding.pry
   end
 
 end

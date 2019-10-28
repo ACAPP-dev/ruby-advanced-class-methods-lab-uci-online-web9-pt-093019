e636153e93a55a8bc12ea809e9375347b63e6206
Andrew Capp <acapp909@gmail.com>
2019-10-28 02:00:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,13 +35,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    create_by_name(name) unless find_by_name(name)
-    #if find_by_name(name)
-    #  find_by_name(name)
-    #else
-    #  create_by_name(name)
-    #end
-    #binding.pry
+    if find_by_name(name)
+      find_by_name(name)
+    else
+      create_by_name(name)
+    end
   end
 
 end
